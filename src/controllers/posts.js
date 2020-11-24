ac564cd884a8b5899b2bd53037f04f966323d729
unknown <amit22666@gmail.com>
2020-11-24 22:26:43 +0530

    posts controller made
    Signed-off-by: unknown <amit22666@gmail.com>

---
+++ src/controllers/posts.js
@@ -0,0 +1,58 @@
+const { Posts, Users } = require('../db/models')
+
+async function createNewPost(userId, title, body) {
+  const post = await Posts.create({
+    title,
+    body,
+    userId,
+  })
+
+  return post
+}
+
+/**
+ * showAllPosts({username: ''})
+ * showAllPosts({title: ''})
+ */
+async function findAllPosts(query) {
+
+  let where = {}
+  console.log(query.userId)
+  if (query.userId) { where.userId = query.userId }
+  const posts = await Posts.findAll({
+    include: [ Users ],
+    where
+  })
+
+  return posts
+}
+
+module.exports = {
+  createNewPost,
+  findAllPosts
+}
+
+/* Test Code */
+
+// async function task() {
+//   // console.log(
+//   //   await createNewPost(
+//   //     1,
+//   //     'This is a sample post',
+//   //     'Body of the post goes here'
+//   //   )
+//   // ),
+//   // console.log(
+//   //   await createNewPost(
+//   //     2,
+//   //     'Another sample post',
+//   //     'Some body example here as well'
+//   //   )
+//   // )
+//   const posts = await findAllPosts()
+//   for (let p of posts) {
+//     console.log(`${p.title}\nauthor: ${p.user.username}\n${p.body}\n==========\n`)
+//   }
+// }
+
+// task()
